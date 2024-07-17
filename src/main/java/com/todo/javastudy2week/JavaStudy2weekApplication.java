b2ab2a6eb218e674ecd7c37da5e7fc0be3629a6b
seunghwan <lovelyunsh@naver.com>
2024-07-17 17:31:58 +0900

    init commit

---
+++ src/main/java/com/todo/javastudy2week/JavaStudy2weekApplication.java
@@ -0,0 +1,13 @@
+package com.todo.javastudy2week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStudy2weekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStudy2weekApplication.class, args);
+    }
+
+}
